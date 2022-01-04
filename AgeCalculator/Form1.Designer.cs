c7865989eff1b63b763926e73807341a12bdd6c9
- <->
2022-01-04 21:32:27 +0700

    Add project files.

---
+++ AgeCalculator/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace AgeCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpDOB = new System.Windows.Forms.DateTimePicker();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpCurrentDate = new System.Windows.Forms.DateTimePicker();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Date of Birth:";
+            // 
+            // dtpDOB
+            // 
+            this.dtpDOB.CustomFormat = "";
+            this.dtpDOB.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDOB.Location = new System.Drawing.Point(97, 28);
+            this.dtpDOB.Name = "dtpDOB";
+            this.dtpDOB.Size = new System.Drawing.Size(200, 20);
+            this.dtpDOB.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(222, 144);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Current Date:";
+            // 
+            // dtpCurrentDate
+            // 
+            this.dtpCurrentDate.CustomFormat = "";
+            this.dtpCurrentDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpCurrentDate.Location = new System.Drawing.Point(97, 67);
+            this.dtpCurrentDate.Name = "dtpCurrentDate";
+            this.dtpCurrentDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpCurrentDate.TabIndex = 4;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(94, 119);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(38, 13);
+            this.lblAge.TabIndex = 5;
+            this.lblAge.Text = "Age: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 191);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.dtpCurrentDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.dtpDOB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Age Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpDOB;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpCurrentDate;
+        private System.Windows.Forms.Label lblAge;
+    }
+}
+
---
+++ AgeCalculator/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgeCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(dtpCurrentDate.Value < dtpDOB.Value)
+                {
+                    MessageBox.Show("Current date must be greater than Date of Brith");
+                }
+                int Age = dtpCurrentDate.Value.Year-dtpDOB.Value.Year;
+                // if add Date of Brith + Age < Current Date
+                if (dtpDOB.Value.AddYears(Age) > dtpCurrentDate.Value)
+                    Age--;
+                lblAge.Text = "Age: " + Age.ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message");
+            }
+        }
+    }
+}
